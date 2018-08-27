b1997ec8012dc8e2d868b4745acdab3c4e17794b
mhaas <mhaas@gfz-potsdam.de>
2018-08-27 12:20:26 +0200

    added shakemap parser; functionality to provide sites and/or roi and/or pgamin; wrapped all in single function

---
+++ quakeml.py
@@ -0,0 +1,141 @@
+#####################################
+# Convert quakeml catalogs to pandas
+# and vice versa
+import pandas
+import lxml.etree as le
+
+#TODO: publicID in quakeml should refer to webservice address
+
+def event2utc(event):
+    '''
+    given event returns UTC string
+    '''
+    d=event.fillna(0)
+    return '{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:09f}Z'.format(int(d.year),int(max(d.month,1)),int(max(d.day,1)),int(d.hour),int(d.minute),d.second)
+
+def utc2event(utc):
+    '''
+    given utc string returns list with year,month,day,hour,minute,second
+    '''
+    #last part usually either Z(ulu) or UTC, if not fails
+    if utc[-3:]=='UTC':
+        utc=utc[:-2]
+    elif utc[-1:]=='Z':
+        pass
+    else:
+        raise Exception('Cannot handle timezone other than Z(ulu) or UTC: {}'.format(utc))
+    date,time = utc.split('T')
+    return [int(v) if i<5 else float(v) for i,v in enumerate([int(d) for d in date.split('-')]+[float(t) for t in time[:-1].split(':')])]
+
+def events2quakeml(catalog,provider='GFZ'):
+    '''
+    Given a pandas dataframe with events returns QuakeML version of
+    the catalog
+    '''
+    #TODO: add uncertainty to all values (NOTE: OQ/HMTK style spatial uncertainty is ellipse semi-major/semi-minor/strike-error)
+    xml_namespace = 'http://quakeml.org/xmlns/quakeml/1.2'
+    quakeml = le.Element('eventParameters',namespace=xml_namespace)
+    #go through all events
+    for i in range(len(catalog)):
+        quake = catalog.iloc[i]
+        event = le.SubElement(quakeml,'event',{'publicID':quake.eventID})
+        preferredOriginID = le.SubElement(event,'preferredOriginID')
+        preferredOriginID.text=quake.eventID
+        preferredMagnitudeID = le.SubElement(event,'preferredMagnitudeID')
+        preferredMagnitudeID.text=quake.eventID
+        qtype = le.SubElement(event,'type')
+        qtype.text = 'earthquake'
+        description = le.SubElement(event,'description')
+        text = le.SubElement(description,'text')
+        text.text = quake.type
+        #origin
+        origin = le.SubElement(event,'origin',{'publicID':quake.eventID})
+        time = le.SubElement(origin,'time')
+        value = le.SubElement(time,'value')
+        value.text = event2utc(quake)
+        latitude = le.SubElement(origin,'latitude')
+        value = le.SubElement(latitude,'value')
+        value.text = str(quake.latitude)
+        longitude = le.SubElement(origin,'longitude')
+        value = le.SubElement(longitude,'value')
+        value.text = str(quake.longitude)
+        depth = le.SubElement(origin,'depth')
+        value = le.SubElement(depth,'value')
+        value.text = str(quake.depth)
+        creationInfo = le.SubElement(origin,'creationInfo')
+        author = le.SubElement(creationInfo,'value')
+        author.text = provider
+        #magnitude
+        magnitude = le.SubElement(event,'magnitude',{'publicID':quake.eventID})
+        mag = le.SubElement(magnitude,'mag')
+        value = le.SubElement(mag,'value')
+        value.text = str(quake.magnitude)
+        mtype = le.SubElement(magnitude,'type')
+        mtype.text = 'MW'
+        creationInfo = le.SubElement(magnitude,'creationInfo')
+        author = le.SubElement(creationInfo,'value')
+        author.text = provider
+        #plane (write only fault plane not auxilliary)
+        focalMechanism = le.SubElement(event,'focalMechanism',{'publicID':quake.eventID})
+        nodalPlanes = le.SubElement(focalMechanism,'nodalPlanes')
+        nodalPlane1 = le.SubElement(nodalPlanes,'nodalPlane1')
+        strike = le.SubElement(nodalPlane1,'strike')
+        value  = le.SubElement(strike,'value')
+        value.text = str(quake.strike)
+        dip = le.SubElement(nodalPlane1,'dip')
+        value  = le.SubElement(dip,'value')
+        value.text = str(quake.dip)
+        rake = le.SubElement(nodalPlane1,'rake')
+        value  = le.SubElement(rake,'value')
+        value.text = str(quake.rake)
+        preferredPlane = le.SubElement(nodalPlanes,'preferredPlane')
+        preferredPlane.text = 'nodalPlane1'
+
+    #return str(le.tostring(quakeml,pretty_print=True,xml_declaration=True),encoding='utf-8')
+    return le.tostring(quakeml,pretty_print=True,encoding='unicode')
+
+def quakeml2events(quakemlfile,provider='GFZ'):
+    '''
+    Given a quakeml file/or string returns a pandas dataframe
+    '''
+    #TODO: add uncertainty
+    try:
+        #read quakeml catalog
+        with open(quakemlfile,'r') as f:
+            quakeml = f.read()
+    except:
+        #maybe already string
+        quakeml = quakemlfile
+
+    quakeml = le.fromstring(quakeml)
+    #initialize catalog
+    index = [i for i in range(len(quakeml))]
+    columns=['eventID', 'Agency', 'Identifier', 'year', 'month', 'day', 'hour', 'minute', 'second', 'timeError', 'longitude', 'latitude',              'SemiMajor90', 'SemiMinor90', 'ErrorStrike', 'depth', 'depthError', 'magnitude', 'sigmaMagnitude','rake','dip','strike','type', 'probability',   'fuzzy']
+    #columns=['eventID', 'Agency', 'Identifier', 'year', 'month', 'day', 'hour', 'minute', 'second', 'timeError', 'longitude', 'latitude','SemiMajor90', 'SemiMinor90', 'ErrorStrike', 'depth', 'depthError', 'magnitude', 'sigmaMagnitude', 'type', 'probability', 'fuzzy']
+    catalog=pandas.DataFrame(index=index,columns=columns)
+    #add individual events to catalog
+    for i,event in enumerate(quakeml):
+        #get ID
+        catalog.iloc[i].eventID = event.attrib['publicID']
+        #type
+        catalog.iloc[i].type = event.find('description').findtext('text')
+        #origin
+        origin = event.find('origin')
+        #time
+        catalog.iloc[i].year,catalog.iloc[i].month,catalog.iloc[i].day,catalog.iloc[i].hour,catalog.iloc[i].minute,catalog.iloc[i].second = utc2event(origin.find('time').findtext('value'))
+        #latitude/longitude/depth
+        catalog.iloc[i].latitude = float(origin.find('latitude').findtext('value'))
+        catalog.iloc[i].longitude = float(origin.find('longitude').findtext('value'))
+        catalog.iloc[i].depth = float(origin.find('depth').findtext('value'))
+        #agency/provider
+        catalog.iloc[i].agency = origin.find('creationInfo').findtext('value')
+        #magnitude
+        catalog.iloc[i].magnitude = float(event.find('magnitude').find('mag').findtext('value'))
+        #plane
+        nodalPlanes = event.find('focalMechanism').find('nodalPlanes')
+        preferredPlane = nodalPlanes.findtext('preferredPlane')
+        catalog.iloc[i].strike = float(nodalPlanes.find(preferredPlane).find('strike').findtext('value'))
+        catalog.iloc[i].dip = float(nodalPlanes.find(preferredPlane).find('dip').findtext('value'))
+        catalog.iloc[i].rake = float(nodalPlanes.find(preferredPlane).find('rake').findtext('value'))
+
+    return catalog
--- service.py
+++ service.py
@@ -9,65 +9,230 @@ from openquake.hazardlib.geo import NodalPlane
 from openquake.hazardlib.pmf import PMF
 from openquake.hazardlib.imt import PGA, PGV, SA
 import shakemap_lite as sml
+import quakeml
+import shakeml
+import pandas
+import argparse
+
+class rpe:
+    '''
+    returns epi-central distance based on simple gmpe
+    '''
+    def __init__(self,c0,c1,c2):
+        self.c0 = c0
+        self.c1 = c1
+        self.c2 = c2
+
+    def get_repi(self,mw,pga):
+        return np.exp((np.log(pga)-self.c0-self.c1*mw)/self.c2)
+
+def get_distance(mw,pga):
+    '''
+    get epicentral distance using
+    simple GMPE
+    ln(pga) = c0 + c1*magnitude + c2*ln(repi)
+    derived from NGA-West2 data
+    '''
+    c0 = -6.553383603995254
+    c1 =  1.6606638130487288
+    c2 = -1.7235855714775898
+    instance = rpe(c0,c1,c2)
+    return instance.get_repi(mw,pga)
+
+def filter_sites(sites,roi):
+    '''
+    filters sites based on roi
+    '''
+    #use pandas dataframe
+    sites = pandas.DataFrame(sites)
+    #filter
+    return sites[(sites.lon >= roi[0]) & (sites.lon <= roi[1]) & (sites.lat >= roi[2]) & (sites.lat <= roi[3])].to_dict('list')
+
+def event_shakemap(quakemlfile,imt = "PGA",gmpe = "BindiEtAl2014Rjb", sites=None, roi=None, pgamin=None):
+    '''
+    Takes an event defined in quakeml: quakemlfile (can be file or string)
+    and returns a shakemap calculated for
+    an intensity type(available in OpenQuake): imt
+    using a GMPE(available in OpenQuake): gmpe
+    at sites that can be defined through:
+     sites: a pandas dataframe with columns lon, lat, vs30, (optional: vs30measured, z1pt0, z2pt5, backarc)
+            Note: can also be a dictionary
+     roi  : a region of interest [lonmin,lonmax,latmin,latmax]
+            (if no additional sites are provided, will use global vs30 grid from USGS), can be used to filter provided sites
+     pgamin: sites can be selected from usgs vs30 grid based on
+             the epicentral distance, where pga typically drops below pgamin (using a simple global GMPE from the NGA-West2)
+             Note: This is only a rough estimate and might be very different from the actual shakemap which may use a more sophisticated GMPE
+     All three ways can be combined also and the intersection of all sets will be used
+     if none of the above is provided the latter is used with a default pgamin of 0.1g
+    '''
+    #read event
+    event = quakeml.quakeml2events(quakemlfile,provider='GFZ')
+    #if more than one limit to first
+    event = event.iloc[0]
+    if pgamin!=None:
+        val=pgamin
+    else:
+        val=0.1
+    #get some idea about distance to be considered for sites
+    #d = 10**np.ceil(np.log10(get_distance(event.magnitude,pgamin)))
+    d = get_distance(event.magnitude,val) #km
+    dlat = d/111.2 #deg
+    dlon = d/111.2*np.cos(event.latitude/180*np.pi) #deg
+    #roi rounded to 0.0001 (gmt precision for grdcut)
+    droi = [event.longitude-dlon,event.longitude+dlon,event.latitude-dlat,event.latitude+dlat]
+    #droi = [round(v,3) for v in droi]
+    #droi = [np.floor((event.longitude-dlon)*10.)/10.,np.ceil((event.longitude+dlon)*10.)/10.,np.floor((event.latitude-dlat)*10.)/10.,np.ceil((event.latitude+dlat)*10.)/10.]
+
+    #check for sites
+    if sites==None:
+        #get sites form USGS topo
+        if roi!=None:
+            sites = sml.get_vs30_sites_from_bbox(roi)
+            #maybe in addition pgamin
+            if pgamin!=None:
+                sites = filter_sites(sites,droi)
+        else:
+            #no sites, no roi
+            #slice sites from vs30
+            sites = sml.get_vs30_sites_from_bbox(droi)
+    else:
+        #sites provided
+        if sites == pandas.core.frame.DataFrame:
+            #convert to dictionary
+            sites = sites.to_dict('list')
+        elif type(sites) == dict:
+            pass
+        else:
+            raise Exception('Unexpected data type for provided sites: {}'.format(type(sites)))
+        #filter in case
+        if roi!=None:
+            sites = filter_sites(sites,roi)
+        if pgamin!=None:
+            #slice sites from vs30
+            sites = sml.get_vs30_sites_from_bbox(droi)
+
+    #create oq event
+    eid = "EQ001"
+    if any(np.isnan(list(event[['longitude','latitude','depth']]))):
+        raise Exception('Need at lest hypocenter of event:\n{}'.format(event))
+    if any(np.isnan(list(event[['strike','dip','rake']]))):
+        oqevent = sml.Event(eid, event.longitude, event.latitude, hypo_depth=event.depth, mag=event.magnitude)
+        rupture = False
+    else:
+        oqevent = sml.Event(eid, event.longitude, event.latitude, hypo_depth=event.depth, mag=event.magnitude, strike=event.strike, dip=event.dip, rake=event.rake)
+        rupture = True
+
+    #generate rupture
+    rupt = oqevent.get_rupture()
+
+    # Setup the shakemap tool with the desired GMPEs and - only needs to be done once.
+    # Note that modification to consider different sets of GMPEs organised by tectonic region is simple
+    shakemap = sml.ShakemapLite("dbfile.hdf5", [gmpe], [imt])
+    # Deletes any existing database of the same name
+    shakemap.reset()
+    # Runs the shakemap for a given event and site configuration
+    shakemap(oqevent, sites)
+
+    #generate a quakemap
+    sm = pandas.DataFrame()
+    sm["LON"] = sites["lon"]
+    sm["LAT"] = sites["lon"]
+    sm["PGA"] = shakemap["{}/{}/{}/{}".format(eid,gmpe,imt,'median')]
+    sm["STDPGA"] = shakemap["{}/{}/{}/{}".format(eid,gmpe,imt,'sigma')]
+
+    #units
+    #FIXME: Check how OQ can return unit for GMPE
+    units = pandas.DataFrame()
+    units["LON"]=['dd']
+    units["LAT"]=['dd']
+    units["PGA"]=['g']
+    units["STDPGA"]=['g']
+    units = units.iloc[0]
+
+    #FIXME: Handle somehow (even necessary)!?
+    event_specific_uncertainty=pandas.DataFrame()
+    event_specific_uncertainty["name"]=['pga', 'pgv', 'mi', 'psa03', 'psa10', 'psa30']
+    event_specific_uncertainty["value"]=[0., 0., 0., 0., 0., 0.]
+    event_specific_uncertainty["numsta"]=["","","","","",""]
+
+    quakemap=shakeml.quakemap(event,event_specific_uncertainty,sm,units)
+
+    #convert to shakeml format and return
+    return shakeml.quakemap2shakeml(quakemap,provider='GFZ')
+
+def main(quakemlfile):
+    print(event_shakemap(quakemlfile))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Return shakemap for quakeml event')
+    parser.add_argument('quakemlfile', help='Quakemlfile')
+    args = parser.parse_args()
+
+    main(args.quakemlfile)
+
+
+
 
 #Get some sites ... here I'm just slicing from the USGS Vs30 topography data, but can be any site model.
 #Should be a dictionary of `{"lon": ..., "lat": ..., "vs30": ...}`
 
 # Get some sites
-sites = sml.get_vs30_sites_from_bbox([5., 7., 47., 49.])
-print(sites)
-plt.figure(figsize=(10,9))
-plt.scatter(sites["lon"], sites["lat"], c=sites["vs30"], edgecolor="None")
-plt.colorbar()
-plt.axis('equal')
-plt.xlim(5., 7.)
-plt.ylim(47., 49)
-plt.title("Vs30 m/s")
-plt.show()
+#sites = sml.get_vs30_sites_from_bbox([5., 7., 47., 49.])
+#print(sites)
+#plt.figure(figsize=(10,9))
+#plt.scatter(sites["lon"], sites["lat"], c=sites["vs30"], edgecolor="None")
+#plt.colorbar()
+#plt.axis('equal')
+#plt.xlim(5., 7.)
+#plt.ylim(47., 49)
+#plt.title("Vs30 m/s")
+#plt.show()
 
 # Build an event - e.g. M 5.5
-eq_lon = 6.0
-eq_lat = 48.0
-eq_depth = 7.0
-mag = 5.5
-event = sml.Event("EQ001", eq_lon, eq_lat, eq_depth, mag)
-print(event)
+#eq_lon = 6.0
+#eq_lat = 48.0
+#eq_depth = 7.0
+#mag = 5.5
+#FIXME:MICHAEL- add rupture plane --> strike,dip,rake
+#if not provided i.e. if NAN use hypocentral distance!
+#event = sml.Event("EQ001", eq_lon, eq_lat, eq_depth, mag)
+#print(event)
 
-# The rupture generation can take place inside the shakemap - or can be input
-rupt = event.get_rupture()
-rlons, rlats = rupt.surface.get_surface_boundaries()
-plt.figure(figsize=(10,9))
-plt.scatter(sites["lon"], sites["lat"], c=sites["vs30"], edgecolor="None")
-plt.plot(rlons[0], rlats[0], 'k-', lw=2)
-plt.plot(eq_lon, eq_lat, "ws")
-plt.colorbar(label="vs30")
-plt.axis('equal')
-plt.xlim(5., 7.)
-plt.ylim(47., 49)
-plt.title("Vs30 m/s")
-plt.show()
+## The rupture generation can take place inside the shakemap - or can be input
+#rupt = event.get_rupture()
+#rlons, rlats = rupt.surface.get_surface_boundaries()
+#plt.figure(figsize=(10,9))
+#plt.scatter(sites["lon"], sites["lat"], c=sites["vs30"], edgecolor="None")
+#plt.plot(rlons[0], rlats[0], 'k-', lw=2)
+#plt.plot(eq_lon, eq_lat, "ws")
+#plt.colorbar(label="vs30")
+#plt.axis('equal')
+#plt.xlim(5., 7.)
+#plt.ylim(47., 49)
+#plt.title("Vs30 m/s")
+#plt.show()
 
 # Run the shakemap - for 3 IMTs and 3 GMPEs
-imts = ["PGA", "SA(0.3)", "SA(1.0)"]
-gmpes = ["BindiEtAl2014Rjb", "CauzziEtAl2014", "AkkarEtAlRjb2014"]
+#imts = ["PGA", "SA(0.3)", "SA(1.0)"]
+#gmpes = ["BindiEtAl2014Rjb", "CauzziEtAl2014", "AkkarEtAlRjb2014"]
 # Setup the shakemap tool with the desired GMPEs and - only needs to be done once.
 # Note that modification to consider different sets of GMPEs organised by tectonic region is simple
-shakemap = sml.ShakemapLite("dbfile.hdf5", gmpes, imts)
-# Deletes any existing database of the same name
-shakemap.reset()
-# Runs the shakemap for a given event and site configuration
-shakemap(event, sites)
+#shakemap = sml.ShakemapLite("dbfile.hdf5", gmpes, imts)
+## Deletes any existing database of the same name
+#shakemap.reset()
+## Runs the shakemap for a given event and site configuration
+#shakemap(event, sites)
 
 
 # To retrieve a particular GMPE and IMT
-bindi_pga = shakemap["EQ001/BindiEtAl2014Rjb/PGA/median"]
-
-plt.figure(figsize=(10,9))
-plt.scatter(sites["lon"], sites["lat"], c=bindi_pga, edgecolor="None", norm=LogNorm(vmin=0.01, vmax=0.3))
-plt.plot(eq_lon, eq_lat, "ws")
-plt.colorbar(label="PGA (g)")
-plt.axis('equal')
-plt.xlim(5., 7.)
-plt.ylim(47., 49)
-plt.show()
+#bindi_pga = shakemap["EQ001/BindiEtAl2014Rjb/PGA/median"]
+#
+#plt.figure(figsize=(10,9))
+#plt.scatter(sites["lon"], sites["lat"], c=bindi_pga, edgecolor="None", norm=LogNorm(vmin=0.01, vmax=0.3))
+#plt.plot(eq_lon, eq_lat, "ws")
+#plt.colorbar(label="PGA (g)")
+#plt.axis('equal')
+#plt.xlim(5., 7.)
+#plt.ylim(47., 49)
+#plt.show()
 
